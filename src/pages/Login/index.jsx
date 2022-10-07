441414121155a2bc33dbf3fea0f11bd476afbf91
lucaspio1 <lucaspioh@gmail.com>
2022-10-07 02:44:05 -0300

    pagina home

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,28 @@
+export const Login = ()=> {
+    return (
+
+            <div>
+               <header className="container flex justify-center max-w-5xl p-4 border-b border-red-300">
+                    <img src="/images/logo-fundo-branco.svg" className="w-32 md:w-40" />
+                </header>
+                <main>
+
+                 <h2 className="text-xl text-red-700 ">Entre na sua conta</h2>
+                 
+                </main>
+               
+
+
+
+
+
+
+
+
+
+
+
+            </div>
+
+    )
+}
